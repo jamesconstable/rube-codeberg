83a75790eda3d539260bfe5dfc24c49c9c8a9a79
James Constable <jwdconstable@gmail.com>
2020-09-06 05:53:38 +1000

    Add maze pattern to map emoji

--- maze_gen.py
+++ maze_gen.py
@@ -21,8 +21,10 @@ def opposite_direction(d):
         return Direction.W
     return Direction.E
 
-def random_suit():
-    return random.choice((' ', ' ', ' ', ' ', '♣️', '♠️', '♥️', '♦️'))
+def random_suit(is_red):
+    if is_red:
+        return random.choice(('♥️', '♦️'))
+    return random.choice((' ', ' ', ' ', '♣️', '♠️'))
 
 def dfs_maze(grid, x, y):
     grid[y][x] = (True, grid[y][x][1], grid[y][x][2])
@@ -39,7 +41,7 @@ def dfs_maze(grid, x, y):
                     grid[j][i][2])
             dfs_maze(grid, i, j)
 
-def maze_box_char(top_left, top_right=NO_WALLS, bottom_left=NO_WALLS):
+def maze_box_char(top_left, top_right, bottom_left):
     if top_left & Direction.E:
         if top_left & Direction.S:
             if bottom_left & Direction.E:
@@ -57,14 +59,23 @@ def maze_box_char(top_left, top_right=NO_WALLS, bottom_left=NO_WALLS):
             return '─' if top_right & Direction.S else '╴'
         return '╶' if top_right & Direction.S else ' '
 
-maze_x = 20
-maze_y = 20
+def bold_box_char(x, y, char):
+    return char
 
+def border(x, y, top_left, top_right=NO_WALLS, bottom_left=NO_WALLS):
+    return bold_box_char(x, y, maze_box_char(top_left, top_right, bottom_left))
+
+# Read in the maze map
+maze_map = open('maze_map.txt', 'r').readlines()
+maze_x = len(maze_map[0])
+maze_y = len(maze_map)
+
+# Initialise blank maze (entirely walls)
 maze = []
 for i in range(maze_y):
     row = []
     for j in range(maze_x):
-        row.append((False, ALL_WALLS, ' '))
+        row.append((False, ALL_WALLS, random_suit(maze_map[i][j]=='#')))
     maze.append(row)
 
 # Add exit point
@@ -73,31 +84,37 @@ maze[maze_y-1][maze_x-1] = (False, ALL_WALLS & ~Direction.E, ' ')
 # Generate the maze
 dfs_maze(maze, 0, 0)
 
-# Print the first row
-print(maze_box_char(NO_WALLS, Direction.S, NO_WALLS), end='')
+# Build the first row
+output = []
+row = []
+row.append(border(0, 0, NO_WALLS, Direction.S, NO_WALLS))
 for x in range(maze_x):
-    print(maze_box_char(Direction.S, Direction.S, NO_WALLS), end='')
-    print(maze_box_char(Direction.S, NO_WALLS if x+1 == maze_x else Direction.S,
-            maze[0][x][1]), end='')
-print()
+    row.append(border(x, 0, Direction.S, Direction.S, NO_WALLS))
+    row.append(border(x, 0, Direction.S,
+            NO_WALLS if x+1 == maze_x else Direction.S, maze[0][x][1]))
+output.append(row)
 
 # Print the rest of the maze. Sorry this is utterly unreadable but I'm on a
 # tight deadline here.
 for y in range(maze_y):
+    row = []
     start_wall = NO_WALLS if y == 0 else Direction.E
-    print(maze_box_char(start_wall, NO_WALLS, start_wall), end='')
+    row.append(border(0, y, start_wall, NO_WALLS, start_wall))
     for x in range(maze_x):
-        print(maze[y][x][2], end='')
+        row.append(maze[y][x][2])
         right_wall = maze[y][x][1] & Direction.E
-        print(maze_box_char(right_wall, NO_WALLS, right_wall), end='')
-    print()
-    print(maze_box_char(start_wall, maze[y][0][1],
-            NO_WALLS if y+1 == maze_y else Direction.E), end='')
+        row.append(border(x, y, right_wall, NO_WALLS, right_wall))
+    output.append(row)
+    row = []
+    row.append(border(maze_x-1, y, start_wall, maze[y][0][1],
+            NO_WALLS if y+1 == maze_y else Direction.E))
     for x in range(maze_x):
         bottom_wall = maze[y][x][1] & Direction.S
-        print(maze_box_char(bottom_wall, bottom_wall), end='')
-        print(maze_box_char(maze[y][x][1],
+        row.append(border(x, y, bottom_wall, bottom_wall))
+        row.append(border(x, y, maze[y][x][1],
                 maze[y][x+1][1] if x+1 < maze_x else NO_WALLS,
-                maze[y+1][x][1] if y+1 < maze_y else Direction.N),
-                end='')
-    print()
+                maze[y+1][x][1] if y+1 < maze_y else Direction.N))
+    output.append(row)
+
+output = '\n'.join(''.join(row) for row in output)
+print(output)
