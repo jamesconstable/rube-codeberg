9f4e6c58657d80e69359974703d4f8bd5063553d
James Constable <jwdconstable@gmail.com>
2020-09-06 14:55:41 +1000

    Fix maze reading and traversal

--- code_only.py
+++ code_only.py
@@ -1,36 +1,64 @@
 #! /usr/bin/env python3
 
 import random
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 def is_wall(x, y, maze):
     return maze[y][x] != ' '
 
-def dfs(x, y, maze):
-    fringe = [(0, 0)]
+def is_end(cell, maze):
+    x, y = cell
+    max_x = len(maze[0])
+    max_y = len(maze)
+    return ((x+2 == max_x) and
+            (y+2 == max_y))
+
+def dfs(maze, visit_fn):
+    max_x = len(maze[0])
+    max_y = len(maze)
+    fringe = [(1, 1)]
     seen = set()
     while len(fringe) > 0:
-        curr = fringe.pop()
-        seen.add(curr)
-        ns = get_neighbours(curr)
+        c = fringe.pop()
+        x, y = c
+        if c in seen:
+            continue
+        seen.add(c)
+        visit_fn(c, maze)
+        if is_end(c, maze):
+            return
+        ns = neighbours(c, maze)
         for n in ns:
-            if n in seen:
-                continue
-            fringe.append(curr)
+            fringe.append(n)
+
+message = []
+def write_cell(cell, maze):
+    x, y = cell
+    if maze[y][x] not in ' █':
+        message.append(maze[y][x])
+
+def neighbours(cell, maze):
+    x, y = cell
+    if maze[y-1][x] == ' ':
+        yield (x, y-2)
+    if maze[y][x+1] == ' ':
+        yield (x+2, y)
+    if maze[y+1][x] == ' ':
+        yield (x, y+2)
+    if maze[y][x-1] == ' ':
+        yield (x-2, y)
 
 f = open('rube_codeberg.py', 'r')
-maze = f.readlines()[1:]
+maze = f.readlines()
 f.close()
 
 for i, line in enumerate(maze):
     maze[i] = line.split('#')[-1]
     maze[i] = list(maze[i])
 
-for y in range(1, len(maze), 2):
-    for x in range(1, len(maze[y]), 2):
-        if maze[y][x] in ' .^"':
-            maze[y][x] = random.choice('abcdefghijklmnpqrstuvwxyz' +
-               'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        print(maze[y])
-
-print(''.join(''.join(m) for m in maze))
+dfs(maze, write_cell)
+message = ''.join(message)
+print(message.replace('.', ' ')
+        .replace('"', '')
+        .replace('^', '')
+        .rstrip())
